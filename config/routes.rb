0bc9322620054dc83ea0e22d7ddaa8be1e728f33
thamzhengyi <thamzhengyi@gmail.com>
2015-09-20 14:35:39 +0800

    Installation of devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :requests
   resources :offers
   resources :cars
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
