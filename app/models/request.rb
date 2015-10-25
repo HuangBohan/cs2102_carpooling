4c07044f3ca8f4c134ea855efd613e2f40691b5f
Louis Lai <hoangdung2506@gmail.com>
2015-10-26 01:46:45 +0800

    Validation for request

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,11 @@ class Request < ActiveRecord::Base
   self.primary_keys = :offer_datetime, :offer_car_license_plate_number, :requester_username
   belongs_to :requester, class_name: :User, foreign_key: :requester_username
   belongs_to :offer, foreign_key: [:offer_datetime, :offer_car_license_plate_number]
+
+  validate :enough_money
+  def enough_money
+    return if current_user.credits >= offer.cost
+    errors.add(:request, 'must have enough money')
+  end
+
 end
