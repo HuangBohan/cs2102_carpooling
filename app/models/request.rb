a62259585dc0080e960404de2b4f86956120c305
Louis Lai <hoangdung2506@gmail.com>
2015-10-25 23:06:13 +0800

    Fix request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 class Request < ActiveRecord::Base
-  self.primary_keys = :offer_datetime, :offer_car_license_plate_number, :requester_email
+  self.primary_keys = :offer_date_time, :offer_car_license_plate_number, :requester_email
   belongs_to :requester, class_name: :User, foreign_key: :requester_email
-  belongs_to :offer, foreign_key: [:offer_datetime, :offer_car_license_plate_number]
+  belongs_to :offer, foreign_key: [:offer_date_time, :offer_car_license_plate_number]
   validate :at_least_one_seat
 
   private
